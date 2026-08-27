440d532bb41ed5c4758dc73fd88e4920acc0ce00
Beltran, Nicolás <nicolasaiyajin0@gmail.com>
2026-08-27 08:53:29 -0300

    Update juego_memoria.sql

--- docs/juego_memoria.sql
+++ docs/juego_memoria.sql
@@ -17,17 +17,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de datos: `juego_memoria`
---
-CREATE DATABASE IF NOT EXISTS `juego_memoria` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-USE `juego_memoria`;
+CREATE DATABASE IF NOT EXISTS `memory_game` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `memory_game`;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `conjuntos_de_cartas`
---
 
 DROP TABLE IF EXISTS `conjuntos_de_cartas`;
 CREATE TABLE `conjuntos_de_cartas` (
@@ -38,11 +30,6 @@ CREATE TABLE `conjuntos_de_cartas` (
   `sprite_base` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `jugadores`
---
 
 DROP TABLE IF EXISTS `jugadores`;
 CREATE TABLE `jugadores` (
@@ -51,11 +38,6 @@ CREATE TABLE `jugadores` (
   `apellido` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `pares`
---
 
 DROP TABLE IF EXISTS `pares`;
 CREATE TABLE `pares` (
@@ -65,12 +47,6 @@ CREATE TABLE `pares` (
   `valor_pareja` varchar(100) DEFAULT NULL,
   `imagen` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `partida`
---
 
 DROP TABLE IF EXISTS `partida`;
 CREATE TABLE `partida` (
@@ -82,12 +58,6 @@ CREATE TABLE `partida` (
   `JUGADORES_ID` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ranking`
---
-
 DROP TABLE IF EXISTS `ranking`;
 CREATE TABLE `ranking` (
   `ID` int NOT NULL,
@@ -96,12 +66,6 @@ CREATE TABLE `ranking` (
   `dificultad_del_nivel` varchar(50) DEFAULT NULL,
   `mejor_tiempo` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `registros_intento_de_volteo`
---
 
 DROP TABLE IF EXISTS `registros_intento_de_volteo`;
 CREATE TABLE `registros_intento_de_volteo` (
@@ -112,12 +76,6 @@ CREATE TABLE `registros_intento_de_volteo` (
   `timestamp` datetime NOT NULL,
   `fue_acierto` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `resultado_final`
---
 
 DROP TABLE IF EXISTS `resultado_final`;
 CREATE TABLE `resultado_final` (
@@ -130,12 +88,6 @@ CREATE TABLE `resultado_final` (
   `aciertos` int DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tableros_generados`
---
-
 DROP TABLE IF EXISTS `tableros_generados`;
 CREATE TABLE `tableros_generados` (
   `ID` int NOT NULL,
@@ -146,154 +98,82 @@ CREATE TABLE `tableros_generados` (
   `tamaño_filas` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Índices para tablas volcadas
---
 
---
--- Indices de la tabla `conjuntos_de_cartas`
---
 ALTER TABLE `conjuntos_de_cartas`
   ADD PRIMARY KEY (`ID`);
 
---
--- Indices de la tabla `jugadores`
---
 ALTER TABLE `jugadores`
   ADD PRIMARY KEY (`ID`);
 
---
--- Indices de la tabla `pares`
---
 ALTER TABLE `pares`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `FK_PAR_CONJUNTO` (`CDC_ID`);
 
---
--- Indices de la tabla `partida`
---
 ALTER TABLE `partida`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `FK_PARTIDA_JUGADOR` (`JUGADORES_ID`);
 
---
--- Indices de la tabla `ranking`
---
 ALTER TABLE `ranking`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `FK_RANKING_PARTIDA` (`PARTIDAS_ID`);
 
---
--- Indices de la tabla `registros_intento_de_volteo`
---
 ALTER TABLE `registros_intento_de_volteo`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `FK_REGISTRO_PARTIDA` (`PARTIDA_ID`);
 
---
--- Indices de la tabla `resultado_final`
---
 ALTER TABLE `resultado_final`
   ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `UQ_RESULTADO_PARTIDA` (`PARTIDA_ID`);
 
---
--- Indices de la tabla `tableros_generados`
---
+
 ALTER TABLE `tableros_generados`
   ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `UQ_TABLERO_PARTIDA` (`PARTIDA_ID`),
   ADD KEY `FK_TABLERO_CONJUNTO` (`CDC_ID`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `conjuntos_de_cartas`
---
 ALTER TABLE `conjuntos_de_cartas`
   MODIFY `ID` int NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `jugadores`
---
 ALTER TABLE `jugadores`
   MODIFY `ID` int NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `pares`
---
 ALTER TABLE `pares`
   MODIFY `ID` int NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `partida`
---
+
 ALTER TABLE `partida`
   MODIFY `ID` int NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `ranking`
---
 ALTER TABLE `ranking`
   MODIFY `ID` int NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `registros_intento_de_volteo`
---
 ALTER TABLE `registros_intento_de_volteo`
   MODIFY `ID` int NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `resultado_final`
---
 ALTER TABLE `resultado_final`
   MODIFY `ID` int NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `tableros_generados`
---
 ALTER TABLE `tableros_generados`
   MODIFY `ID` int NOT NULL AUTO_INCREMENT;
 
---
--- Restricciones para tablas volcadas
---
 
---
--- Filtros para la tabla `pares`
---
 ALTER TABLE `pares`
   ADD CONSTRAINT `FK_PAR_CONJUNTO` FOREIGN KEY (`CDC_ID`) REFERENCES `conjuntos_de_cartas` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Filtros para la tabla `partida`
---
+
 ALTER TABLE `partida`
   ADD CONSTRAINT `FK_PARTIDA_JUGADOR` FOREIGN KEY (`JUGADORES_ID`) REFERENCES `jugadores` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Filtros para la tabla `ranking`
---
+
 ALTER TABLE `ranking`
   ADD CONSTRAINT `FK_RANKING_PARTIDA` FOREIGN KEY (`PARTIDAS_ID`) REFERENCES `partida` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Filtros para la tabla `registros_intento_de_volteo`
---
 ALTER TABLE `registros_intento_de_volteo`
   ADD CONSTRAINT `FK_REGISTRO_PARTIDA` FOREIGN KEY (`PARTIDA_ID`) REFERENCES `partida` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Filtros para la tabla `resultado_final`
---
 ALTER TABLE `resultado_final`
   ADD CONSTRAINT `FK_RESULTADO_PARTIDA` FOREIGN KEY (`PARTIDA_ID`) REFERENCES `partida` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Filtros para la tabla `tableros_generados`
---
 ALTER TABLE `tableros_generados`
   ADD CONSTRAINT `FK_TABLERO_CONJUNTO` FOREIGN KEY (`CDC_ID`) REFERENCES `conjuntos_de_cartas` (`ID`) ON DELETE RESTRICT ON UPDATE CASCADE,
   ADD CONSTRAINT `FK_TABLERO_PARTIDA` FOREIGN KEY (`PARTIDA_ID`) REFERENCES `partida` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
