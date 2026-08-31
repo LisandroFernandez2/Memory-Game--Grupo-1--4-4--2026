ac573a9024b0ff2cf2f53cf5c4fb5f264c6c59df
Beltran, Nicolás <nicolasaiyajin0@gmail.com>
2026-08-31 16:55:54 -0300

    Update memory_game.sql

--- docs/memory_game.sql
+++ docs/memory_game.sql
@@ -17,8 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE DATABASE IF NOT EXISTS `memory_game` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-USE `memory_game`;
+CREATE DATABASE IF NOT EXISTS `juego_memoria` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `juego_memoria`;
 
 
 DROP TABLE IF EXISTS `conjuntos_de_cartas`;
